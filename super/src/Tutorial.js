cc85e89acd9456942f9fc8a4a9cfa38ddf45947b
Rodion Yermakov <rodion.yermakov.sd@gmail.com>
2024-04-24 14:44:27 -0700

    daily commit

--- super/src/Tutorial.js
+++ super/src/Tutorial.js
@@ -21,5 +21,20 @@ createRoot(root).render(<h1>Hello World</h1>);
 
 <input max={2} disabled={true} className="textbox" />
 
+import clsx from "clsx";
 
+function MyComponent(props) {
+    const className = clsx({
+        "text": true, // this class is always added
+        "title": props.loggedIn
+    });
+
+    return <h1 className={className}></h1>
+}
+
+
+function Navbar(props) {
+    const {children, ...rest} = props;
+    return <h1 {...rest}>{children}</h1>;
+}
 */
