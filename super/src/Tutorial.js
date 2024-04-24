368d2899a27f404dd77bb1db490adb1088842a8f
Rodion Yermakov <rodion.yermakov.sd@gmail.com>
2024-04-23 22:10:26 -0700

    daily commit

---
+++ super/src/Tutorial.js
@@ -0,0 +1,25 @@
+// React.createElemnent
+// React.createElement(type, props, children)
+
+/*
+
+
+
+
+import React from "react";
+import {createRoot} from "react-dom/client";
+
+const root = document.querySelector("#root");
+createRoot(root).render(React.createElement("p", {}, "Hello World"));
+
+import React from "react";
+import {createRoot} from "react-dom/client";
+
+const root = document.querySelector("#root");
+createRoot(root).render(<h1>Hello World</h1>);
+
+
+<input max={2} disabled={true} className="textbox" />
+
+
+*/
