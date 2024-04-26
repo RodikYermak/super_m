d106ebee4a0d409ae3dec876ddf10c8ce3e03bed
Rodion Yermakov <rodion.yermakov.sd@gmail.com>
2024-04-26 00:54:06 -0700

    setting up the project

--- super/src/App.js
+++ super/src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload. We are hacking!
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        // <div className="App">
+        //   <header className="App-header">
+        //     <img src={logo} className="App-logo" alt="logo" />
+        //     <p>
+        //       Edit <code>src/App.js</code> and save to reload. We are hacking!
+        //     </p>
+        //     <a
+        //       className="App-link"
+        //       href="https://reactjs.org"
+        //       target="_blank"
+        //       rel="noopener noreferrer"
+        //     >
+        //       Learn React
+        //     </a>
+        //   </header>
+        <></>
+        // </div>
+        // <button className="btn btn-outline" onClick={() => setLoggedIn(false)}>Logout</button>
+        // <h2>Please login</h2>
+        // <button className="btn btn-primary" onClick={() => setLoggedIn(true)}>Login</button>
+    );
 }
 
 export default App;
